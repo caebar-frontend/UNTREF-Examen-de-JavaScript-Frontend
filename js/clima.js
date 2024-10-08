f63a8355d25b3dd1c430aa1bab8f359b63698b15
Cristian Budano <caebar@gmail.com>
2024-10-08 20:54:02 -0300

    Septimo Commit - Punto 2 del archivo clima.js finalizado

--- js/clima.js
+++ js/clima.js
@@ -3,3 +3,9 @@
 const weatherTable = document.getElementById('weatherTable').getElementsByTagName('tbody')[0]
 const historyList = document.getElementById('historyList')
 const clearHistoryBtn = document.getElementById('clearHistoryBtn')
+
+// 2. Definir función para obtener parámetros GET de la URL (ciudad seleccionada)
+function obtenerParametroGET(nombreParametro) {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(nombreParametro);
+}
